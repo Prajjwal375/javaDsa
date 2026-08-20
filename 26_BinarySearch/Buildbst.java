600f1483cccb466c6fd32d88660673c4bd6765f3
PrajvalSaki <166584486+Prajjwal375@users.noreply.github.com>
2026-08-20 11:02:59 +0530

    bst basics questions

---
+++ 26_BinarySearch/Buildbst.java
@@ -0,0 +1,92 @@
+public class Buildbst {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val); // naya node create and bana do bst ka root
+            return root;
+        }
+        if(root.data > val){
+            // left subtree
+            root.left = insert(root.left, val);
+        }else{
+            // right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    ////////////////////////////////////////////
+
+    // delete a node in bst
+    public static Node delete(Node root, int val){
+        if(root.data < val ){
+            root.right = delete(root.right, val);
+        }
+        else if(root.data > val){
+            root.left = delete(root.left, val);
+        }
+        else {
+            // case 1 - leaf Node
+            if(root.left == null && root.right == null){
+                return null;
+            }
+            // case 2 single child
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            // case 3 - two child
+            Node IS = findInorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+    public static Node findInorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+
+    ///////////////////////////////////////
+
+    public static void inorder(Node root){
+        if(root == null){
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+    public static void main(String[] args){
+        int values[] = {8,5,3,1,4,6,10,11,14};
+        Node root = null;
+        for(int i=0; i<values.length; i++){
+            root = insert(root, values[i]);
+        }
+        inorder(root);
+        System.out.println();
+
+        root = delete(root, 1);
+        System.out.println();
+
+        inorder(root);
+       
+
+
+    }
+}
