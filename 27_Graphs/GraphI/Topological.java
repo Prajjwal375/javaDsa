62041b9a1e51550d46b12cfead54990cd01eb497
PrajvalSaki <166584486+Prajjwal375@users.noreply.github.com>
2026-09-02 16:17:50 +0530

    bfs topological sort

--- 27_Graphs/GraphI/Topological.java
+++ 27_Graphs/GraphI/Topological.java
@@ -61,11 +61,54 @@ public class Topological {
 
 
     ////////////////////////////////////////////////////////////////////////////////
+    ///
+    ///
+    
+    // Topological sort using BFS
+    public static void topSortBFS(ArrayList<Edge>[] graph){
+        int indeg[] = new int[graph.length];
+        calcIndeg(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
+        
+        for(int i=0; i<indeg.length; i++){
+            if(indeg[i] == 0){
+                q.add(i);
+            }
+        }
+
+        // bfs
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            System.out.print(curr + " "); // topological sort
+
+            for(int i=0; i<graph[curr].size(); i++){
+                Edge e = graph[curr].get(i);
+                indeg[e.dest]--;
+                if(indeg[e.dest] == 0){
+                    q.add(e.dest);
+                }
+            }
+        }
+        System.out.println();
+    }
+    
+    public static void calcIndeg(ArrayList<Edge>[] graph, int indeg[]){
+        for(int i=0; i<graph.length; i++){
+            for(int j=0; j<graph[i].size(); j++){
+                Edge e = graph[i].get(j);
+                indeg[e.dest]++;
+            }
+        }  
+    }
+
+
 
     public static void main(String args[]){
         int V = 5;
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
+
+        
 
     } 
 
